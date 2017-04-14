b969062fe63afd3deeee178914c2bf1ed9a200ae
aduric <adnan.duric@d2l.com>
2017-04-14 19:03:17 -0400

    Updates packages

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, 
         loaders: [
-          "babel"
+          "babel-loader"
         ]
       },
       {test: /node_modules[\\\/]auth0-lock[\\\/].*\.js$/,
