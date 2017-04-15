a29e946f8903b4f95e611a70f6c2e0b2e2112973
aduric <adnan.duric@d2l.com>
2017-04-15 08:44:58 -0400

    Uses new router

--- source/index.js
+++ source/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import createHistory from 'history/createBrowserHistory'
 import vrsvsApp from './reducers'
 import App from './components/App'
 import NotFound from './components/NotFound'
@@ -12,8 +13,8 @@ import VisibleChallengeList from './containers/VisibleChallengeList'
 import initialUsers from '../data/users.json'
 import initialChallenges from '../data/challenges.json'
 import config from '../config.json'
-import { Router, Route, browserHistory, Link, IndexRoute, IndexRedirect } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
+import { Route, Link, IndexRoute, IndexRedirect } from 'react-router'
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import AuthService from './util/AuthService'
 import {persistStore, autoRehydrate} from 'redux-persist'
@@ -22,10 +23,15 @@ import {persistStore, autoRehydrate} from 'redux-persist'
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const store = createStore(vrsvsApp, { "users": initialUsers, "challenges": initialChallenges }, autoRehydrate());
-persistStore(store)
+const history = createHistory()
+const middleware = routerMiddleware(history)
 
-const history = syncHistoryWithStore(browserHistory, store);
+const store = createStore(
+  vrsvsApp, 
+  { "users": initialUsers, "challenges": initialChallenges }, 
+  compose(
+    applyMiddleware(middleware), 
+    autoRehydrate()));
 
 const auth = new AuthService(config.auth0key, config.auth0uri);
 
@@ -38,15 +44,14 @@ const requireAuth = (nextState, replace) => {
 
 render(
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App} auth={auth}>
-        <IndexRedirect to="users" />
-        <IndexRoute component={VisibleUserList}/>
-        <Route path="login" component={Login}/>
-        <Route path="users" component={VisibleUserList}/>
-        <Route path="challenges" component={VisibleChallengeList}/>
-      </Route>
-    </Router>
+    <ConnectedRouter history={history}>
+      <div>
+        <Route path="/" component={App} auth={auth} />
+        <Route path="/login" component={Login}/>
+        <Route path="/users" component={VisibleUserList}/>
+        <Route path="/challenges" component={VisibleChallengeList}/>
+      </div>
+    </ConnectedRouter>
   </Provider>
 , document.getElementById('root')
 )
