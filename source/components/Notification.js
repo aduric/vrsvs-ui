e3f0cdef7942be6a5e50994c990a57d819baffe2
aduric <adnan.duric@d2l.com>
2017-05-23 00:25:42 -0400

    Adds date to notifications

--- source/components/Notification.js
+++ source/components/Notification.js
@@ -10,10 +10,13 @@ import {List, ListItem} from 'material-ui/List';
 
 const Notification = (props) => ({
   render() {
-    const { resource, message, subject, object } = this.props;
+    const { resource, message, subject, object, timestamp } = this.props;
+    const d = new Date(timestamp);
+    d.
     return(
       <ListItem
         primaryText={eval(message)}
+        secondaryText={`${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()}`}
         leftAvatar={<svg style={{width: "48px", height: "48px"}}>
               <use xlinkHref='svg/vrsvs_icon.svg#Bear_logo'/>
             </svg>}
